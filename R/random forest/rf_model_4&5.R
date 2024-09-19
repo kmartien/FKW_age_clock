fdcb1f86ca1a6a1caeb78442b48656e5eefa094e
kmartien <115281269+kmartien@users.noreply.github.com>
2024-09-19 16:52:02 +0000

    Re-ran RF site selection with no weighting. Separate runs using all samples and only CR4&5 samples. Chose optimum values of mtry and sampsize for both sets of training samples (All and CR4&5) with all sites and with only RF important sites.

--- R/random forest/rf_model_4&5.R
+++ R/random forest/rf_model_4&5.R
@@ -3,10 +3,41 @@ library(tidyverse)
 library(randomForest)
 library(rfPermute)
 
-load("R/data/age_and_methylation_data.rdata")
+load("data/age_and_methylation_data.rdata")
+
+#' run randomForest over sampsize and mtry grid 
+#' and report deviance stats
+rf.param.grid.search <- function(model.df, sites){
+  # grid of sampsize and mtry to optimize MSE over
+  params <- expand.grid(
+    sampsize = seq(round(nrow(model.df)*.3), round(nrow(model.df)*.7), by = 1), 
+    mtry = seq(round(length(sites)*.2), round(length(sites)*.5), by = 1), 
+    KEEP.OUT.ATTRS = FALSE
+  )
+  
+  parallel::mclapply(1:nrow(params), function(i){
+    rf <- randomForest(
+      y = model.df$age.best,
+      x = model.df[, sites.to.keep],
+      mtry = params$mtry[i],
+      ntree = 10000,
+      sampsize = params$sampsize[i],
+      replace = FALSE
+    )
+    data.frame( 
+      sampsize = params$sampsize[i],
+      mtry = params$mtry[i],
+      mse = rf$mse[length(rf$mse)],
+      rsq = rf$rsq[length(rf$rsq)],
+      pct.var = 100 * rf$rsq[length(rf$rsq)]
+    )
+  }, mc.cores = 6) |> 
+    bind_rows() |> 
+    as.data.frame()
+}
 
 model.df <- age.df |>
-  filter(swfsc.id %in% ids.to.keep & age.confidence >= 4) |>
+  filter(swfsc.id %in% ids.to.keep & age.confidence >=4) |>
   left_join(
     logit.meth.normal.params |>
       filter(loc.site %in% sites.to.keep) |>
@@ -15,60 +46,41 @@ model.df <- age.df |>
     by = 'swfsc.id'
   ) |> 
   column_to_rownames('swfsc.id') |> 
-  mutate(var.wt = 1 / age.var)
+  mutate(inv.var = 1 / age.var)
 
-params <- expand.grid(
-  sampsize = seq(floor(nrow(model.df)/4), round(nrow(model.df)*0.632), by = 2), 
-  mtry = seq(30, 70, by = 2), 
-  KEEP.OUT.ATTRS = FALSE
-)
+param.df  <- rf.param.grid.search(model.df, sites.to.keep)
+save(param.df, file = "R/random forest/RF.CR4&5.Allsites.grid.search.rda")
 
-#' run randomForest over sampsize and mtry grid 
-#' and report deviance stats
-param.df <- parallel::mclapply(1:nrow(params), function(i){
-  rf <- randomForest(
-    y = model.df$age.best,
-    x = model.df[, sites.to.keep],
-    mtry = params$mtry[i],
-    ntree = 10000,
-    sampsize = params$sampsize[i],
-    replace = FALSE
-  )
-  data.frame( 
-    sampsize = params$sampsize[i],
-    mtry = params$mtry[i],
-    mse = rf$mse[length(rf$mse)],
-    rsq = rf$rsq[length(rf$rsq)],
-    pct.var = 100 * rf$rsq[length(rf$rsq)]
-  )
-}, mc.cores = 6) |> 
-  bind_rows() |> 
-  as.data.frame()
-
-plot <- param.df |> 
+# plot heatmap of MSE across grid
+param.df |> 
   ggplot() +
   geom_tile(aes(sampsize, mtry, fill = mse)) +
-  scale_fill_distiller(palette = 'YlOrRd')
+  scale_fill_distiller(palette = 'RdYlBu', direction = 1)
 
-rf.params <- filter(param.df, mse == min(param.df$mse)) |>
+rf.params <- list()
+rf.params$Allsites <- filter(param.df, mse == min(param.df$mse)) |>
   select(c(mtry, sampsize))
-save(rf.params, file = 'R/random forest/rf_optim_params_CR4&5_All.rda')
+#rf.params$Allsites$mtry <- 75
+#rf.params$Allsites$sampsize <- 35
+save(rf.params, file = 'R/random forest/rf_optim_params_CR4&5.rda')
 
+# run rfPermute at sampsize and mtry with minimum MSE
 rp <- rfPermute(
   y = model.df$age.best,
   x = model.df[, sites.to.keep],
-  mtry = rf.params$mtry,
-  ntree = 10000,
-  sampsize = rf.params$sampsize,
+  mtry = rf.params$Allsites$mtry,
+  ntree = 20000,
+  sampsize = rf.params$Allsites$sampsize,
   importance = TRUE,
   replace = FALSE,
   num.rep = 1000,
   num.cores = 10
 )
 
+save.image('R/random forest/rf_model_CR4&5.rdata')
+
 # save site importance scores and p-values
-rf_site_importance_CR4_5 <- 
-  rp |> 
+rp |> 
   importance() |> 
   as.data.frame() |> 
   rownames_to_column('loc.site') |> 
@@ -76,55 +88,56 @@ rf_site_importance_CR4_5 <-
     incMSE = '%IncMSE',
     pval = '%IncMSE.pval'
   ) |> 
-  select(loc.site, incMSE, pval)  
-  saveRDS(rf_site_importance_CR4_5, 'R/random forest/rf_site_importance_CR4&5.rds')
+  select(loc.site, incMSE, pval) |> 
+  saveRDS('R/random forest/rf_site_importance_CR4&5.rds')
 
-print(rp)
-plotTrace(rp)
-plotInbag(rp)
-plotImportance(rp)
-plotImportance(rp, sig = TRUE)
-
-#' re-run randomForest over sampsize and mtry grid with only important sites
-#' and report deviance stats
-
-sites <- rf_site_importance_CR4_5 |> 
+# select important sites from Random Forest
+sites <- readRDS('R/random forest/rf_site_importance_CR4&5.rds') |> 
   filter(pval <= 0.1) |> 
   pull('loc.site')
 
-params <- expand.grid(
-  sampsize = seq(floor(nrow(model.df)/4), round(nrow(model.df)*0.632), by = 2), 
-  mtry = seq(floor(length(sites)*0.15), round(length(sites)*.4), by = 1), 
-  KEEP.OUT.ATTRS = FALSE
-)
+param.df  <- rf.param.grid.search(model.df, sites)
+save(param.df, file = "R/random forest/RF.CR4&5.RFsites.grid.search.rda")
 
-#' run randomForest over sampsize and mtry grid 
-#' and report deviance stats
-param.df <- parallel::mclapply(1:nrow(params), function(i){
-  rf <- randomForest(
-    y = model.df$age.best,
-    x = model.df[, sites],
-    mtry = params$mtry[i],
-    ntree = 10000,
-    sampsize = params$sampsize[i],
-    replace = FALSE
-  )
-  data.frame( 
-    sampsize = params$sampsize[i],
-    mtry = params$mtry[i],
-    mse = rf$mse[length(rf$mse)],
-    rsq = rf$rsq[length(rf$rsq)],
-    pct.var = 100 * rf$rsq[length(rf$rsq)]
-  )
-}, mc.cores = 6) |> 
-  bind_rows() |> 
-  as.data.frame()
-
+# plot heatmap of MSE across grid
 param.df |> 
   ggplot() +
   geom_tile(aes(sampsize, mtry, fill = mse)) +
-  scale_fill_distiller(palette = 'YlOrRd')
+  scale_fill_distiller(palette = 'RdYlBu', direction = 1)
 
-rf.params <- filter(param.df, mse == min(param.df$mse)) |>
+rf.params$RFsites <- filter(param.df, mse == min(param.df$mse)) |>
   select(c(mtry, sampsize))
-save(rf.params, file = 'R/random forest/rf_optim_params_CR4&5_RFsites.rda')
+#rf.params$RFsites$mtry <- 24
+#rf.params$RFsites$sampsize <- 34
+save(rf.params, file = 'R/random forest/rf_optim_params_CR4&5.rda')
+
+# model diagnostics
+print(rp)
+plotTrace(rp)
+plotImportance(rp, imp = '%IncMSE', alpha = 0.2)
+plotImportance(rp, sig = TRUE)
+
+model.df |> 
+  mutate(
+    age.est = rp$rf$predicted,
+    age.confidence = factor(age.confidence)
+  ) |> 
+  ggplot() +
+  geom_abline(intercept = 0, slope = 1) +
+  geom_point(aes(age.best, age.est, color = age.confidence), size = 3) +
+  geom_segment(aes(x = age.min, xend = age.max, y = age.est, yend = age.est, color = age.confidence), alpha = 0.5) +
+  scale_color_manual(values = conf.colors) +
+  theme(legend.position = 'top')
+
+age.pred <- rp$rf$predicted |> 
+  enframe(name = 'swfsc.id', value = 'age.pred') |> 
+  left_join(age.df, by = 'swfsc.id') |> 
+  mutate(mse = (age.best - age.pred) ^ 2)
+
+age.pred |> 
+  mutate(age.confidence = as.character(age.confidence)) |> 
+  group_by(age.confidence) |> 
+  summarize(mean.mse = mean(mse), .groups = 'drop') |> 
+  bind_rows(
+    data.frame(age.confidence = 'All', mean.mse = mean(age.pred$mse))
+  )
