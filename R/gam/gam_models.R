9c887f26f1ca9689405ac1005cd0b8cad728c25e
eric.archer <eric.archer@noaa.gov>
2024-09-23 11:06:25 -0700

    gam model rerun based on new RF important sites (Allsamps and CR4and5 models)

--- R/gam/gam_models.R
+++ R/gam/gam_models.R
@@ -4,10 +4,8 @@ library(mgcv)
 source('../misc_funcs.R')
 load("../../data/age_and_methylation_data.rdata")
 
-# select important sites from Random Forest
-sites <- readRDS('../random forest/rf_site_importance.rds') |> 
-  filter(pval <= 0.1) |> 
-  pull('loc.site')
+nrep <- 1000
+ncores <- 10
 
 # subset all data
 age.df <- age.df |>  
@@ -22,43 +20,80 @@ model.df <- age.df |>
     by = 'swfsc.id'
   ) 
 
-nrep <- 5000
-ncores <- 10
-
-
-# Best age and methylation estimates --------------------------------------
-
+# filter for CR 4 & 5 samples for training model data
 train.df <- filter(model.df, age.confidence %in% 4:5)
-predictAllIDsGAM(train.df, model.df, sites, 'age.best') |> 
-  saveRDS('gam_best.rds')
 
 
-# Random age and best methylation estimates -------------------------------
 
+# Using sites from RF trained on all samples ------------------------------
+
+# select important sites from Random Forest trained on all samples
+sites_Allsamps <- readRDS('../random forest/rf_site_importance_Allsamps.rds') |> 
+  filter(pval <= 0.05) |> 
+  pull('loc.site')
+
+# best age and methylation
+message(format(Sys.time()), ' : Best - All')
+predictAllIDsGAM(train.df, model.df, sites_Allsamps, 'age.best') |> 
+  saveRDS('gam_best_Allsamps.rds')
+
+# random age and best methylation
+message(format(Sys.time()), ' : RanAge - All')
 parallel::mclapply(1:nrep, function(j) {
-  # random sample of ages and methylation - only use random age
+  ran.df <- model.df |> 
+    left_join(
+      sampleAgeMeth(age.df, logit.meth.normal.params) |> 
+        select(swfsc.id, age.ran),
+      by = 'swfsc.id'
+    ) 
+  predictAllIDsGAM(filter(ran.df, age.confidence %in% 4:5), ran.df, sites_Allsamps, 'age.ran')
+}, mc.cores = ncores) |> 
+  bind_rows() |> 
+  saveRDS('gam_ran_age_Allsamps.rds')
+
+# random age and random methylation
+message(format(Sys.time()), ' : RanAgeMeth - All')
+parallel::mclapply(1:nrep, function(j) {
+  ran.df <- sampleAgeMeth(age.df, logit.meth.normal.params) 
+  predictAllIDsGAM(filter(ran.df, age.confidence %in% 4:5), ran.df, sites_Allsamps, 'age.ran')
+}, mc.cores = ncores) |> 
+  bind_rows() |> 
+  saveRDS('gam_ran_age_meth_Allsamps.rds')
+
+
+
+
+# Using sites from RF trained on CR 4 & 5 samples -------------------------
+
+# select important sites from Random Forest trained on CR 4 & 5 samples
+sites_CR4and5 <- readRDS('../random forest/rf_site_importance_CR4&5.rds') |> 
+  filter(pval <= 0.05) |> 
+  pull('loc.site')
+
+# best age and methylation
+message(format(Sys.time()), ' : Best - CR4and5')
+predictAllIDsGAM(train.df, model.df, sites_CR4and5, 'age.best') |> 
+  saveRDS('gam_best_CR4and5.rds')
+
+# random age and best methylation
+message(format(Sys.time()), ' : RanAge - CR4and5')
+parallel::mclapply(1:nrep, function(j) {
   ran.df <- model.df |> 
     left_join(
       sampleAgeMeth(age.df, logit.meth.normal.params) |> 
         select(swfsc.id, age.ran),
       by = 'swfsc.id'
     )
-  
-  train.df <- filter(ran.df, age.confidence %in% 4:5)
-  predictAllIDsGAM(train.df, ran.df, sites, 'age.ran')
+  predictAllIDsGAM(filter(ran.df, age.confidence %in% 4:5), ran.df, sites_CR4and5, 'age.ran')
 }, mc.cores = ncores) |> 
   bind_rows() |> 
-  saveRDS('gam_ran_age.rds')
+  saveRDS('gam_ran_age_CR4and5.rds')
 
-
-# Random age and random methylation estimates -----------------------------
-
+# random age and random methylation
+message(format(Sys.time()), ' : RanAgeMeth - CR4and5')
 parallel::mclapply(1:nrep, function(j) {
-  # random sample of ages and methylation
   ran.df <- sampleAgeMeth(age.df, logit.meth.normal.params) 
-  
-  train.df <- filter(ran.df, age.confidence %in% 4:5)
-  predictAllIDsGAM(train.df, ran.df, sites, 'age.ran')
+  predictAllIDsGAM(filter(ran.df, age.confidence %in% 4:5), ran.df, sites_CR4and5, 'age.ran')
 }, mc.cores = ncores) |> 
   bind_rows() |> 
-  saveRDS('gam_ran_age_meth.rds')
+  saveRDS('gam_ran_age_meth_CR4and5.rds')
