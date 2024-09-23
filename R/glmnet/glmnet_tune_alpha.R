513812d881fd067d44004753db9b73e20e9973a3
kmartien <115281269+kmartien@users.noreply.github.com>
2024-09-23 18:10:58 +0000

    glmnet.chosen.sites was corrupt because I changed the extension, now it's fixed

--- R/glmnet/glmnet_tune_alpha.R
+++ R/glmnet/glmnet_tune_alpha.R
@@ -44,16 +44,17 @@ glmnet.optim <- lapply(c(2,4), function(minCR){
   train.df <- filter(model.df, age.confidence >= minCR)
 
   # find optimal alpha (lowest median cvm at minimum lambda)
-  optimum.alpha <- optim(
-    par = c(alpha = 0.3),
-    fn = median.cvm.min,
-    method = "Brent",
-    lower = 0.0001,
-    upper = 0.5,
-    df = train.df,
-    nrep = 1000
-  )
-  optimum.alpha
+  # opt.alpha <- optim(
+  #   par = c(alpha = 0.3),
+  #   fn = median.cvm.min,
+  #   method = "Brent",
+  #   lower = 0.0001,
+  #   upper = 0.5,
+  #   df = train.df,
+  #   nrep = 1000
+  # )
+  label <- paste0('minCR', minCR)
+  opt.alpha <- optimum.alpha[[label]]
   
   # multiple LOO runs of the glmnet model with alpha at lowest cvm
   fit <- parallel::mclapply(1:nrep, function(i) {
@@ -61,19 +62,19 @@ glmnet.optim <- lapply(c(2,4), function(minCR){
       CVglmnet10K(
         train.df, 
         sites.to.keep, 
-        optimum.alpha$par
+        opt.alpha$par
       )
     }, error = function(e) NULL)
   }, mc.cores = 10)
   
-  return(list(optimum.alpha = optimum.alpha, fit = fit))
+  return(list(optimum.alpha = opt.alpha, fit = fit))
 })
 names(glmnet.optim) <- c('minCR2', 'minCR4')
 
 lapply(glmnet.optim, function(i){i$optimum.alpha}) |>
   saveRDS(file = 'R/glmnet/optimum.alpha.rds')
 
-glmnet.chosen.sites <- lapply(glmnet.optim, function(minCR){
+lapply(glmnet.optim, function(minCR){
   do.call(bind_rows,
   lapply(minCR$fit, function(i){
     coeffs <- coef(i, s = 'lambda.min') 
